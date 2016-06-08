dc5034cf618f9c95b96e0905184677b3d0592955
Charlie Baxter and Shradha Pulla <shradha@>
2016-06-08 15:19:37 -0700

    Define string as an array, and utilize .indexOf method to make the fifth specification work

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ The program should add "ay" to words that begin with a vowel.
   Example Input: animal
   Example Output: animalay
 
+For words that start with "qu", move the "qu" to the end of the word, then add "ay".
+  Example Input: quarter
+  Example Output: arterquay
+
 For words that begin with a single consonant, the program should move the first consonant to the end of the word, then add "ay".
   Example Input: cat
   Example Output: atcay
@@ -12,6 +16,6 @@ For words that start with a "y", treat as a consonant. Move the first consonant 
   Example Input: yam
   Example Output: amyay
 
-For words that start with "qu", move the "qu" to the end of the word, then add "ay".
-  Example Input: quarter
-  Example Output: arterquay
+For words that start with two or more consonants, the program should move the consonants to the end of the word, then add "ay".
+  Example Input: think
+  Example Output: inkthay
--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,66 @@
-
-// business logic
-
-
-
-// user interface logic
 $(document).ready(function() {
   $("form#pig-latin").submit(function(event) {
     event.preventDefault();
-    var phrase = $("input#phrase").val();
-    var result = phrase;
-    var phraseSlice = phrase.slice(0, 1);
-    var phraseQ = phrase.slice(0, 2);
+    var phraseInput = $("input#phrase").val();
+    var results = phraseInput.split("");
+    var phraseSlice = phraseInput.slice(0,1);
+    var phraseQ = phraseInput.slice(0, 2);
+    var newPhrase = "";
 
-    if ((phrase[0] === "a") || (phrase[0] === "e") || (phrase[0] === "i") || (phrase[0] === "o") || (phrase[0] === "u")){
+    if ((phrase[0] !== "a") || (phrase[0] !== "e") || (phrase[0] !== "i") || (phrase[0] !== "o") || (phrase[0] !== "u")) {
+      results.forEach(function(phrase) {
+        if ((phrase === "a") || (phrase === "e") || (phrase === "i") || (phrase === "o") || (phrase === "u")) {
+          var vowelIndex = results.indexOf(phrase);
+          phraseSlice = phraseInput.slice(0,vowelIndex);
+          var endSlice = phraseInput.slice(vowelIndex, phraseInput.length);
+          result = endSlice + phraseSlice + "ay";
+        } else {
+        }
+// Above statement meets third and fourth and fifth specifications
+      });
+    } else if ((phrase[0] === "a") || (phrase[0] === "e") || (phrase[0] === "i") || (phrase[0] === "o") || (phrase[0] === "u")) {
       result = phrase + "ay";
+// Above statement meets first specification
     } else if ((phrase[0] === "q") && (phrase[1] === "u")) {
-      phrase = phrase.replace(phraseQ,"");
+      phrase = phraseInput.replace(phraseQ,"");
       result = phrase + phraseQ + "ay";
-    } else if ((phrase[0] != "a") || (phrase[0] != "e") || (phrase[0] != "i") || (phrase[0] != "o") || (phrase[0] != "u")) {
-      phrase = phrase.replace(phraseSlice,"");
-      result = phrase + phraseSlice + "ay";
+// Above statement meets second specification
     } else {
-      alert("Doesn't work!");
-    };
-    $(".phrase").text(result);
-    $("#result").show();
-
+        alert("Error");
+    }
+      $(".phrase").text(result);
+      $("#result").show();
   });
 });
+
+
+
+
+
+
+// Older Code
+// $(document).ready(function() {
+//   $("form#pig-latin").submit(function(event) {
+//     event.preventDefault();
+//     var phrase = $("input#phrase").val();
+//     var result = phrase;
+//     var phraseSlice = phrase.slice(0,1);
+//     var phraseQ = phrase.slice(0, 2);
+//     var newPhrase = "";
+//
+//     if ((phrase[0] === "a") || (phrase[0] === "e") || (phrase[0] === "i") || (phrase[0] === "o") || (phrase[0] === "u")) {
+//       result = phrase + "ay";
+// // Above statement meets first specification
+//     } else if ((phrase[0] === "q") && (phrase[1] === "u")) {
+//       phrase = phrase.replace(phraseQ,"");
+//       result = phrase + phraseQ + "ay";
+// // Above statement meets second specification
+//     } else if ((phrase[0] != "a") || (phrase[0] != "e") || (phrase[0] != "i") || (phrase[0] != "o") || (phrase[0] != "u")) {
+//       phrase = phrase.replace(phraseSlice,"");
+//       result = phrase + phraseSlice + "ay";
+//       }
+//     // Above statement meets third and fourth specifications
+//       $(".phrase").text(result);
+//       $("#result").show();
+//   });
+// });
