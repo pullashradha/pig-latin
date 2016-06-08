b2399419500b8e25236b0e8462faf52438cba96e
Charlie Baxter and Shradha Pulla <charlie@>
2016-06-08 10:58:14 -0700

    Add js code to fulfill second specification in README.md

--- js/scripts.js
+++ js/scripts.js
@@ -9,9 +9,13 @@ $(document).ready(function() {
     event.preventDefault();
     var phrase = $("input#phrase").val();
     var result = phrase;
+    var phraseSlice = phrase.slice(0, 1);
 
     if ((phrase[0] === "a") || (phrase[0] === "e") || (phrase[0] === "i") || (phrase[0] === "o") || (phrase[0] === "u")){
       result = phrase + "ay";
+    } else if ((phrase[0] != "a") || (phrase[0] != "e") || (phrase[0] != "i") || (phrase[0] != "o") || (phrase[0] != "u")) {
+      phrase = phrase.replace(phraseSlice,"");
+      result = phrase + phraseSlice + "ay";
     } else {
       alert("Doesn't work!");
     };
